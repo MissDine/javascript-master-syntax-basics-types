7ef75fc34b50f7c323546d622f927e67de968005
MissDine <enidnyatichi1212@gmail.com>
2022-05-30 11:24:13 +0300

    Feat:Edit

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ let year = 2022
 console.log(age, year);
 age = 30;
 console.log(age);
+//Please note that you cannot start a word with a  number - however u can use all the other characters like letters,and the rest.
