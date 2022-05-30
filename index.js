1aaae5e1fd6af00082c17b35c3f547ec2a83e626
MissDine <enidnyatichi1212@gmail.com>
2022-05-30 12:12:34 +0300

    Feat:Edit

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ console.log(age);
 //Please note that you cannot start a word with a  number - however u can use all the other characters like letters,and the rest.
 
 // Data types
-// 1.Numbers- 1,2,4 etc
-// 2.String-name enid,hello etc
+// 1.Numbers- 1,2,4,100,0.3,22.9 etc
+// 2.String-"name" "enid","hello" etc
 // 3.Boolean-true or false
-// 4.Null-
+// 4.Null-Explitly set a  variable has no value eg age = null
 // 5.Undefined-
 // 6.Object- {name:enid,age:25}etc
 // 7.Symbol-
